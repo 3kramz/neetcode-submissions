62f13ff5e7098dcf668d8d53bda4fe16dd597258
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2026-08-08 13:15:21 +0600

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        if (s.length==1){
+
+            return true
+        }
+        
+        let l = 0
+        let r = s.length-1
+
+        while (l < r) {
+            while (l < r && !this.alphaNum(s[l])) { l++ }
+
+            while (r > l && !this.alphaNum(s[r])) r--
+            if (s[l].toLowerCase() != s[r].toLowerCase()) {
+                return false
+            }
+            l++
+            r--
+        }
+        return true
+
+    }
+
+    alphaNum(c) {
+        return (
+            (c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z') ||
+            (c >= '0' && c <= '9')
+        );
+    }
+}
